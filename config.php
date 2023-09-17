d3f6f63507763a86d0e35d0885faffb99fd073de
Larissa <larissalopez3001@gmail.com>
2023-09-17 17:45:45 -0500

    first commit

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+session_start();
+define('RUTA', 'http://localhost/sistema/');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_NAME', 'sistema');
+define('DB_PASS', '12345');
+?>
